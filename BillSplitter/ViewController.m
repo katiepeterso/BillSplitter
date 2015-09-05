d51703fc33dab06e19c68c163b1ce8d60e782a32
katiepeterso <katiepeterso@outlook.com>
2015-09-05 13:35:26 -0700

    add logic for calculation

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -14,6 +14,8 @@
 @property (strong, nonatomic) IBOutlet UISlider *numberOfPeople;
 @property (strong, nonatomic) IBOutlet UISlider *percentTip;
 @property (strong, nonatomic) IBOutlet UILabel *amountEach;
+@property (strong, nonatomic) IBOutlet UILabel *numberOfPeopleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *percentTipLabel;
 
 @end
 
@@ -29,12 +31,71 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)typingBillAmount:(UITextField *)sender {
+- (IBAction)typeBillAmount:(UITextField *)sender {
     
+    NSDecimalNumber *bill = [NSDecimalNumber decimalNumberWithString:sender.text];
+    
+    NSNumber *tip = [NSNumber numberWithFloat:(100.00 + self.percentTip.value)/100.00];
+    NSDecimalNumber *decTip = [NSDecimalNumber decimalNumberWithDecimal:[tip decimalValue]];
+    
+    NSNumber *people = [NSNumber numberWithFloat:self.numberOfPeople.value];
+    NSDecimalNumber *decPeople = [NSDecimalNumber decimalNumberWithDecimal:[people decimalValue]];
+    
+    NSDecimalNumber *withTip = [bill decimalNumberByMultiplyingBy:decTip];
+    NSDecimalNumber *totalEach = [withTip decimalNumberByDividingBy:decPeople];
+    
+    NSNumberFormatter *formatter = [NSNumberFormatter new];
+    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    self.amountEach.text = [formatter stringFromNumber:totalEach];
 }
 
-- (IBAction)calculateSplit:(UISlider *)sender {
+- (IBAction)selectNumberOfPeople:(UISlider *)sender {
     
+    if ([self.billAmount.text length] == 0) {
+        self.numberOfPeopleLabel.text = [[NSString stringWithFormat:@"%.0f", sender.value] stringByAppendingString:@" People"];
+    }
+    else {
+        sender = self.numberOfPeople;
+        NSDecimalNumber *bill = [NSDecimalNumber decimalNumberWithString:self.billAmount.text];
+
+        NSNumber *tip = [NSNumber numberWithFloat:(100.00 + self.percentTip.value)/100.00];
+        NSDecimalNumber *decTip = [NSDecimalNumber decimalNumberWithDecimal:[tip decimalValue]];
+        
+        NSNumber *people = [NSNumber numberWithFloat:sender.value];
+        NSDecimalNumber *decPeople = [NSDecimalNumber decimalNumberWithDecimal:[people decimalValue]];
+        
+        NSDecimalNumber *withTip = [bill decimalNumberByMultiplyingBy:decTip];
+        NSDecimalNumber *totalEach = [withTip decimalNumberByDividingBy:decPeople];
+        
+        NSNumberFormatter *currencyFormatter = [NSNumberFormatter new];
+        [currencyFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+        self.amountEach.text = [currencyFormatter stringFromNumber:totalEach];
+        self.numberOfPeopleLabel.text = [[NSString stringWithFormat:@"%.0f", sender.value] stringByAppendingString:@" People"];
+    }
+}
+
+- (IBAction)selectTipPercentage:(UISlider *)sender {
+    if ([self.billAmount.text length] == 0) {
+        self.percentTipLabel.text = [[NSString stringWithFormat:@"%.0f", sender.value] stringByAppendingString:@"%"];
+    }
+    else {
+        sender = self.percentTip;
+        NSDecimalNumber *bill = [NSDecimalNumber decimalNumberWithString:self.billAmount.text];
+        
+        NSNumber *tip = [NSNumber numberWithFloat:(100.00 + sender.value)/100.00];
+        NSDecimalNumber *decTip = [NSDecimalNumber decimalNumberWithDecimal:[tip decimalValue]];
+        
+        NSNumber *people = [NSNumber numberWithFloat:self.numberOfPeople.value];
+        NSDecimalNumber *decPeople = [NSDecimalNumber decimalNumberWithDecimal:[people decimalValue]];
+        
+        NSDecimalNumber *withTip = [bill decimalNumberByMultiplyingBy:decTip];
+        NSDecimalNumber *totalEach = [withTip decimalNumberByDividingBy:decPeople];
+        
+        NSNumberFormatter *currencyFormatter = [NSNumberFormatter new];
+        [currencyFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+        self.amountEach.text = [currencyFormatter stringFromNumber:totalEach];
+        self.percentTipLabel.text = [[NSString stringWithFormat:@"%.0f", sender.value] stringByAppendingString:@"%"];
+    }
 }
 
 @end
